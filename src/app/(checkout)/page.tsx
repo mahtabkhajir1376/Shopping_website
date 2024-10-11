4989c12fc60d7a745eb9bc60c361e8135f8343f4
mahtabkhajir1376 <mahtabkhajir0@gmail.com>
2024-10-11 16:21:05 +0330

    create cart page and checkoutpage

---
+++ src/app/(checkout)/page.tsx
@@ -0,0 +1,9 @@
+// // Handles the checkout process and payment details.
+
+import React from "react";
+
+const Checkout: React.FC = () => {
+  return <div>Checkout page</div>;
+};
+
+export default Checkout;
