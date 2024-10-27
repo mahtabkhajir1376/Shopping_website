2fc32fd0359d39698c55c8a9c05d93951268b810
mahtabkhajir1376 <mahtabkhajir0@gmail.com>
2024-10-27 10:51:32 +0330

    create magazine dynamic page

---
+++ src/app/(main)/magazine/[magazineId]/page.tsx
@@ -0,0 +1,9 @@
+// This dynamic page displays content for a specific magazine issue based on its ID .
+
+import React from "react";
+
+const MagazinePage: React.FC = () => {
+  return <div>page</div>;
+};
+
+export default MagazinePage;
